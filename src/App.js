962a7070a56f3f42239193c5336e475c0f6d4d22
Daniel Felizardo <daniel.david772@gmail.com>
2020-04-08 03:00:34 -0300

    Repositories Web

--- src/App.js
+++ src/App.js
@@ -1,31 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
-function App() {
+export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function loadRepositories() {
+      try {
+        const response = await api.get('repositories')
+
+        setRepositories(response.data);
+      } catch(err) {
+        console.error(err);
+      }
+    }
+    loadRepositories()
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    try {
+      const response = await api.post('repositories', {
+        title: `Repo ${Date.now()}`,
+        url: 'https://github.com/danieldfc/bootcamp-gostack-react-desafio-02',
+        techs: ['ReactJS'],
+      });
+
+      setRepositories([...repositories, response.data]);
+    } catch(err) {
+      console.error(err);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`repositories/${id}`);
+
+      const results = repositories.filter(repository => repository.id !== id);
+
+      setRepositories(results);
+    } catch(err) {
+      console.error(err);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories && repositories.map(repository => (
+          <li key={repository.id}>
+            <h1>{repository.title}</h1>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
-
-export default App;
